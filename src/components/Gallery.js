d10cd7be86001d8f2eb0180d2b5f7e70bdd26190
AydinKaseyinoff <kaseyinov@gmail.com.com>
2024-05-01 23:49:43 +0600

    change name of image and add it

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navigation from "./Navigation";
 import image1 from "./img/image1.jpg";
+import image2 from "./img/image2.jpg";
+import image3 from "./img/image3.jpg";
+import image4 from "./img/image4.jpg";
 
 const Gallery = () => {
   return (
@@ -14,6 +17,9 @@ const Gallery = () => {
       <div className="gallery-images">
         {/* Здесь можно добавить изображения для галереи школы */}
         <img src={image1} alt="Мое изображение" />
+        <img src={image2} alt="Мое изображение" />
+        <img src={image3} alt="Мое изображение" />
+        <img src={image4} alt="Мое изображение" />
         {/* Дополнительные изображения */}
       </div>
     </div>
