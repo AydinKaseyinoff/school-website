0523e31ee0a29b8e24ff88fa3479e6ddfeb7ed4e
AydinKaseyinoff <kaseyinov@gmail.com.com>
2024-05-03 01:45:13 +0600

    upgrade teachers pge

--- src/components/TeachersPage.js
+++ src/components/TeachersPage.js
@@ -1,6 +1,12 @@
 import React from "react";
 import TeacherCard from "./TeacherCard"; // Импортируем компонент TeacherCard
 import teacher1 from "./teach-img/teacher1.jpg";
+import teacher2 from "./teach-img/teacher2.jpg";
+import teacher3 from "./teach-img/teacher3.jpg";
+import teacher4 from "./teach-img/teacher4.jpg";
+import teacher5 from "./teach-img/teacher5.jpg";
+import teacher6 from "./teach-img/teacher6.jpg";
+import teacher7 from "./teach-img/teacher7.jpg";
 import "./TeachersPage.css";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
@@ -22,36 +28,43 @@ const TeachersPage = () => {
         contact="+996700999999"
       />
       <TeacherCard
-        image={teacher1}
+        image={teacher2}
         name="Жасынжанов Тураржан"
         experience="5 лет"
-        subject="Кыргыз тил/Кыргыз Адабияты"
+        subject="Директор орун басары"
         contact="+996700999999"
       />
       <TeacherCard
-        image={teacher1}
-        name="Касымова Айнура"
+        image={teacher3}
+        name="Качаганова Жылдыз"
         experience="10 лет"
-        subject="Кыргыз тил/Кыргыз Адабияты"
-        contact="+996700999999"
+        subject="Тарбия завуч"
+        contact="+996500031346"
       />
       <TeacherCard
-        image={teacher1}
-        name="Мантекова Кундуз"
+        image={teacher4}
+        name="Сыргаков Кыялбек"
         experience="10 лет"
-        subject="Кыргыз тил/Кыргыз Адабияты"
+        subject="соц Педагог"
         contact="+996700999999"
       />
       <TeacherCard
-        image={teacher1}
-        name="Жаныш кызы Бегимай"
+        image={teacher5}
+        name="Джаналиева Айгул"
         experience="10 лет"
-        subject="Кыргыз тил/Кыргыз Адабияты"
+        subject="Орустил Мугалими"
         contact="+996700999999"
       />
       <TeacherCard
-        image={teacher1}
-        name="Джумабаева Махабат Муптуевна"
+        image={teacher6}
+        name="Отонбаева Анара"
+        experience="10 лет"
+        subject="Биология мугалими"
+        contact="+996700999999"
+      />
+      <TeacherCard
+        image={teacher7}
+        name="Кожоканова Рыскул"
         experience="10 лет"
         subject="Кыргыз тил/Кыргыз Адабияты"
         contact="+996700999999"
